726d7e04cceba8b581789392fbe51748b773a112
John Sissler <jsissler@gmail.com>
2019-05-09 17:18:08 -0400

    First commit, incomplete.

---
+++ Block Breaker/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    public void LoadNextScene()
+    {
+        int currentActiveScene = SceneManager.GetActiveScene().buildIndex;
+        if (currentActiveScene + 1 < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(currentActiveScene + 1);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
