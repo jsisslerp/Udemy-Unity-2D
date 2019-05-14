7bc2a86484b062838355f207675dc1346002a79d
John Sissler <jsissler@gmail.com>
2019-05-14 17:15:54 -0400

    Up through walls.

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    [SerializeField] private Paddle paddle1;
+    [SerializeField] private float xPush = 2f;
+    [SerializeField] private float yPush = 15f;
+
+    private Vector2 paddleToBallVector;
+    private bool hasStarted = false;
+
+	// Use this for initialization
+	void Start () {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 15f);
+            hasStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+    [SerializeField] private float screenWidthInUnits = 16f;
+    [SerializeField] private float minX = 1f;
+    [SerializeField] private float maxX = 15f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 paddlePos = transform.position;
+        paddlePos.x = Mathf.Clamp(Input.mousePosition.x / Screen.width * screenWidthInUnits, minX, maxX);
+        transform.position = paddlePos;
+	}
+}
