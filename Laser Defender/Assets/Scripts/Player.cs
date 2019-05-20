1b8d39238abaa68b32e6361954371421274db437
John Sissler <jsissler@gmail.com>
2019-05-20 17:06:50 -0400

    Post player movement.

---
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [SerializeField] private float moveSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    private void Move()
+    {
+        var delta = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Time.deltaTime * moveSpeed;
+        transform.position += delta;
+    }
+}
