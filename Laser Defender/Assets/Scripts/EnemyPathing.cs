67c353e558b29294e3df17673772867323167101
John Sissler <jsissler@gmail.com>
2019-05-22 18:12:37 -0400

    Post 98.

---
+++ Laser Defender/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour {
+    [SerializeField] WaveConfig waveConfig;
+    private List<Transform> waypoints;
+    private float moveSpeed = 2f;
+    private int waypointIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+        waypoints = waveConfig.GetWayPoints();
+        moveSpeed = waveConfig.MoveSpeed;
+        transform.position = waypoints[waypointIndex].position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (transform.position == waypoints[waypointIndex].position)
+        {
+            if (++waypointIndex >= waypoints.Count)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].position, moveSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Laser Defender/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject {
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private GameObject pathPrefab;
+    [SerializeField] private float timeBetweenSpawns = 0.5f;
+    [SerializeField] private float spawnRandomFactor = 0.3f;
+    [SerializeField] private int numberOfEnemies = 5;
+    [SerializeField] private float moveSpeed = 0.2f;
+
+    public GameObject EnemyPrefab
+    {
+        get
+        {
+            return enemyPrefab;
+        }
+    }
+
+    public List<Transform> GetWayPoints()
+    {
+        List<Transform> waypoints = new List<Transform>();
+        foreach (Transform child in pathPrefab.transform)
+        {
+            waypoints.Add(child);
+        }
+        return waypoints;
+    }
+
+    public float TimeBetweenSpawns
+    {
+        get
+        {
+            return timeBetweenSpawns;
+        }
+    }
+
+    public float SpawnRandomFactor
+    {
+        get
+        {
+            return spawnRandomFactor;
+        }
+    }
+
+    public int NumberOfEnemies
+    {
+        get
+        {
+            return numberOfEnemies;
+        }
+    }
+
+    public float MoveSpeed
+    {
+        get
+        {
+            return moveSpeed;
+        }
+    }
+}
