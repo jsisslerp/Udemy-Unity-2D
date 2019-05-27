fa88076c952a50d53ed86dc6b5b188c8f0dca6e0
John Sissler <jsissler@gmail.com>
2019-05-27 17:07:13 -0400

    After 125

---
+++ Glitch Garden/Assets/Scripts/NextSceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextSceneLoader : MonoBehaviour {
+    [SerializeField] private float nextSceneDelay = 3f;
+
+	// Use this for initialization
+	IEnumerator Start () {
+        yield return new WaitForSeconds(nextSceneDelay);
+        FindObjectOfType<BuildOrderSceneLoader>().LoadNextScene();
+	}
+}
