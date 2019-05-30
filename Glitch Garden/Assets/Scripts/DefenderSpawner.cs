5b819c9f09dca8667d0c79421d33ef350200f337
John Sissler <jsissler@gmail.com>
2019-05-30 17:11:51 -0400

    EOD

--- Glitch Garden/Assets/Scripts/DefenderSpawner.cs
+++ Glitch Garden/Assets/Scripts/DefenderSpawner.cs
@@ -14,7 +14,7 @@ public class DefenderSpawner : MonoBehaviour {
     Vector2 GetSquareClicked()
     {
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-        return new Vector2(Mathf.Round(worldPos.x), Mathf.Round(worldPos.y));
+        return new Vector2(Mathf.RoundToInt(worldPos.x), Mathf.RoundToInt(worldPos.y));
     }
 
     private void SpawnDefender(Vector2 worldPos)
