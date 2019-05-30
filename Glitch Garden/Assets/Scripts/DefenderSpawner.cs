aaf13040e6dc40698cc5184f0f773011de6cd728
John Sissler <jsissler@gmail.com>
2019-05-30 15:13:52 -0400

    Post 140

---
+++ Glitch Garden/Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DefenderSpawner : MonoBehaviour {
+    [SerializeField] private GameObject defender;
+
+    private void OnMouseDown()
+    {
+        SpawnDefender(GetSquareClicked());
+    }
+
+    Vector2 GetSquareClicked()
+    {
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+        return new Vector2(Mathf.Round(worldPos.x), Mathf.Round(worldPos.y));
+    }
+
+    private void SpawnDefender(Vector2 worldPos)
+    {
+        Instantiate(defender, worldPos, Quaternion.identity);
+    }
+}
