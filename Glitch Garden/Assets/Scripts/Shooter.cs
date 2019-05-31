f25857f1b8179096c34150f05f60846227ee840b
John Sissler <jsissler@gmail.com>
2019-05-31 19:35:14 -0400

    EOD

--- Glitch Garden/Assets/Scripts/Shooter.cs
+++ Glitch Garden/Assets/Scripts/Shooter.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour {
     [SerializeField] private GameObject projectile, gun;
+    private AttackerSpawner myLaneSpawner;
+    private Animator animator;
 
 	// Use this for initialization
 	void Start () {
-		
+        foreach(AttackerSpawner spawner in FindObjectsOfType<AttackerSpawner>())
+        {
+            if (Mathf.RoundToInt(spawner.transform.position.y) == Mathf.RoundToInt(transform.position.y))
+            {
+                myLaneSpawner = spawner;
+                break;
+            }
+        }
+
+        animator = GetComponent<Animator>();
 	}
 
     public void Fire()
@@ -17,6 +28,13 @@ public class Shooter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (myLaneSpawner.transform.childCount > 0)
+        {
+            animator.SetBool("IsAttacking", true);
+        }
+        else
+        {
+            animator.SetBool("IsAttacking", false);
+        }
+    }
 }
