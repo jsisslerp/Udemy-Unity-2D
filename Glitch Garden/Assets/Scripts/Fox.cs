02646cd50a0cbafbb0561189b4d56dcd25bfff33
John Sissler <jsissler@gmail.com>
2019-06-03 12:51:30 -0400

    Post 158

---
+++ Glitch Garden/Assets/Scripts/Fox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject gameObject = collision.gameObject;
+
+        if (gameObject.GetComponent<GraveStone>())
+        {
+            GetComponent<Animator>().SetTrigger("JumpTrigger");
+        }
+        else if (gameObject.GetComponent<Defender>())
+        {
+            GetComponent<Attacker>().Attack(gameObject);
+        }
+    }
+}
---
+++ Glitch Garden/Assets/Scripts/GraveStone.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraveStone : MonoBehaviour {
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        
+    }
+}
